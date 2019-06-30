9e48e9bd4195319492f99ed9e6bf4e1dabfcfd70
Shiqiu2019 <snowchang02@gmail.com>
2019-06-30 17:23:42 +0800

    mytest

--- myapp.py
+++ myapp.py
@@ -7,11 +7,12 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 
-def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html; charset=utf-8')])
+   
+def index():
+    """ Displays the index page accessible at '/'
+    """
     return flask.render_template('index.html')
 
-   
 
 if __name__ == '__main__':
     # app.debug=True
