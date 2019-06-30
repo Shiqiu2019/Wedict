beeb47712537d66b9d3ea1d570eeefc6c06c447f
Shiqiu2019 <snowchang02@gmail.com>
2019-06-30 13:34:07 +0800

    mytest

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,1 @@
-def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html; charset=utf-8')])
-    return ['<!DOCTYPE html><html><meta charset="utf-8"><title>It works',
-            '</title><h1>It works!</h1>']
+from myapp import app as application
