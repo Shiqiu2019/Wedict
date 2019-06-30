711aa512b6faa67adaea6b8ff9e8fa0c6d28d122
Shiqiu2019 <snowchang02@gmail.com>
2019-06-30 16:15:17 +0800

    mytest

--- myapp.py
+++ myapp.py
@@ -1,11 +1,17 @@
-from flask import Flask
-app = Flask(__name__)
+import flask
 
-@app.route('/')
-def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html; charset=utf-8')])
-    return ['<!DOCTYPE html><html><meta charset="utf-8"><title>It works',
-            '</title><h1>It works!</h1>']
+
+# Create the application.
+APP = flask.Flask(__name__)
+
+
+@APP.route('/')
+def index():
+    """ Displays the index page accessible at '/'
+    """
+    return flask.render_template('index.html')
+
 
 if __name__ == '__main__':
-    app.run()
+    # APP.debug=True
+    APP.run()
