bf242bfeb5237cce699cefee19a1a67fe6fceb9b
Shiqiu2019 <snowchang02@gmail.com>
2019-06-30 15:14:36 +0800

    mytest

--- myapp.py
+++ myapp.py
@@ -2,8 +2,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World! 20190630  here'
+def application(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/html; charset=utf-8')])
+    return ['<!DOCTYPE html><html><meta charset="utf-8"><title>It works',
+            '</title><h1>It works!</h1>']
 
 if __name__ == '__main__':
     app.run()
