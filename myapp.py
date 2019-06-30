73e8712bdaaab2476f8f420958ac35cf98f215b9
Shiqiu2019 <snowchang02@gmail.com>
2019-06-30 17:59:01 +0800

    mytest

--- myapp.py
+++ myapp.py
@@ -7,13 +7,10 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 
-   
-def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html; charset=utf-8')])
-    return ['<!DOCTYPE html><html><meta charset="utf-8"><title>It works',
-            '</title><h1>It works!</h1>']
+def application():
+    return flask.render_template('index.html')
 
-            
+
 if __name__ == '__main__':
     # app.debug=True
     app.run()
