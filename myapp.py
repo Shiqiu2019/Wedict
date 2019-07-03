2ab261bfa416974d12bf8512b7013067da10cc6a
Shiqiu2019 <snowchang02@gmail.com>
2019-07-03 23:50:14 +0800

    myblog

--- myapp.py
+++ myapp.py
@@ -293,7 +293,7 @@ def not_found(exc):
 
 def main():
     database.create_tables([Entry, FTSEntry], safe=True)
-    app.run(debug=True)
+    # app.run(debug=True)
 
 if __name__ == '__main__':
     main()
